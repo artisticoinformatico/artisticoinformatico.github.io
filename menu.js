62be47a6b8f2ea79d1459e94a38329f365fc4066
artisticoinformatico <96482901+artisticoinformatico@users.noreply.github.com>
2022-02-11 09:05:12 +0100

    dasd
    dasds

--- menu.js
+++ menu.js
@@ -9,6 +9,7 @@ document.write(
 '<a href="'+adres+'map.html"><img src="'+adres+'menu/map.png" alt="map" title="Map"></a>'+
 '<a href="'+adres+'kontakt.html"><img src="'+adres+'menu/kontakt.png" alt="kontakt" title="Kontakt"></a>'+
 '<a href="'+adres+'hobby.html"><img src="'+adres+'menu/hobby.png" alt="hobby" title="Hobby"></a>'+
+'<a href="'+adres+'ankieta.html"><img src="'+adres+'menu/ankieta.png" alt="ankieta" title="Ankieta"></a>'+
 '<a href="'+adres+'videło.html"><img src="'+adres+'menu/video.png" alt="Video" title="Video"></a>'+
 '<a href="'+adres+'galeria/thumb.html"><img src="'+adres+'menu/galeria.png" alt="galeria" title="Galeria"></a>'+
 '<a href="'+adres+'js.html"><img src="'+adres+'menu/javascript.png" alt="javascript" title="Javascript"></a>'+
