6eab2ddedc5b40a1fac38c8369927cff85249bb7
artisticoinformatico <96482901+artisticoinformatico@users.noreply.github.com>
2022-03-17 14:30:00 +0100

    lokj

---
+++ javascript/f_czasu.js
@@ -0,0 +1,87 @@
+function nazwa_miesiaca(nrm){
+
+    if(nrm==0) return "Stycznia";
+    if(nrm==1) return "Lutego";
+    if(nrm==2) return "Marca";
+    if(nrm==3) return "Kwietnia";
+    if(nrm==4) return "Maja";
+    if(nrm==5) return "Czerwca";
+    if(nrm==6) return "Lipca";
+    if(nrm==7) return "Sierpnia";
+    if(nrm==8) return "WrzeĹnia";
+    if(nrm==9) return "PaĹşdziernika";
+    if(nrm==10) return "Listopada";
+    if(nrm==11) return "Grudnia";
+}
+function dzien(dzn){
+    if(dzn==0) return "Niedziela";
+    if(dzn==1) return "PoniedziaĹek";
+    if(dzn==2) return "Wtorek";
+    if(dzn==3) return "Ĺroda";
+    if(dzn==4) return "Czwartek";
+    if(dzn==5) return "PiÄtek";
+    if(dzn==6) return "Sobota";
+}
+
+    //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//funkcja zwraca date w postaci rrrr-mm-dd
+function DataOdDo(data){
+
+
+
+    let mies=data.getMonth()+1;
+    let dzien=data.getDate();
+    
+    return data.getFullYear()+'-'+
+    (mies<10?'0'+mies:mies)+'-'+
+    (dzien<10?'0'+dzien:dzien);
+    }
+
+    function pisz_data(){
+
+        let data=new Date();
+        let sek=data.getSeconds();
+        let min=data.getMinutes();
+        let godz=data.getHours();
+
+        let wypis=("<h2>"+"DziĹ jest: "+dzien(data.getDay())+(" ")+data.getDate()+(" ")+
+        nazwa_miesiaca(data.getMonth())+(" ")+data.getFullYear()+"<br>"+
+        
+        "Jest godzina: ");
+        
+        if(godz<10) wypis+='0'+godz+(":");
+        else wypis+=godz+(":");
+        
+        if(min<10) wypis+='0'+min+(":");
+        else wypis+=min+(":");
+        
+        if(sek<10) wypis+='0'+sek;
+        else wypis+=sek;
+
+        let data_pobrana=document.getElementById('data_pobrana').value;
+
+        
+        wypis+='<br> Data z pola: ';
+
+            let Year=data_pobrana.substr(0,4);
+            let Month=data_pobrana.substr(5,2)-1;
+            let Day=data_pobrana[8]+data_pobrana[9];
+        
+            let data_zpola=new Date(Year, Month, Day);
+            wypis+=dzien(data_zpola.getDay())+' '+data_zpola.getDate()+' '+nazwa_miesiaca(data_zpola.getMonth())+ ' '+data_zpola.getFullYear()+'</h2>';
+        
+            data_dzis.gettime();
+            data_zpola>gettime();
+        
+            document.getElementById('kom').innerHTML=wypis;
+        
+        
+        }
+
+
+
+
+
+
+        
+        
