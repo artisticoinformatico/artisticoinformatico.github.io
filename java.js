4821825cb9c34f4bcf6de420dfa522c5a7d340d8
artisticoinformatico <96482901+artisticoinformatico@users.noreply.github.com>
2022-03-18 13:03:43 +0100

    asv

---
+++ java.js
@@ -0,0 +1,7 @@
+document.write(
+'<header style="text-align:center; background-color: rgb(218, 144, 34); padding: 25px;>'+
+'<a href="drukarka.html"><img src="pliki_menu/drukarka.png" alt="drukarka" title="Drukarka" style="height; 80px;"></a>'+
+'<a href="dysk.html"><img src="pliki_menu/dysk.png" alt="dysk" title="Dysk" style="height; 80px;"></a>'+
+'<a href="klawiatura.html"><img src="pliki_menu/klawiatura.png" alt="klawiatura" title="Klawiatura" style="height; 80px;"></a>'+
+'</header>'
+);
