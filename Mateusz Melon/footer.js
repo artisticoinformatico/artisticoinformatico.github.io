ce1969a6f1d214be47c6df88ce1cf02599c4b788
artisticoinformatico <96482901+artisticoinformatico@users.noreply.github.com>
2022-03-18 13:04:17 +0100

    ja to kurwa zrobię

---
+++ Mateusz Melon/footer.js
@@ -0,0 +1,8 @@
+document.write(
+    '</section>'+
+    '<footer style="text-align:center; background-color: rgb(218, 144, 34); padding: 25px;">'+
+    '<a href=index.html> Index </a>'+ 
+    '<p id="kom"></p>'+ 
+    '</footer>'+
+    '</div>'
+        );
---
+++ Mateusz Melon/java.js
@@ -0,0 +1,10 @@
+document.write(
+    '<div style="margin: auto; width: 90%;">'+ 
+    '<header style="text-align: center; background-color: rgb(218, 144, 34); padding: 25px;">'+   
+    '<a href="drukarka.html"><img src="pliki_menu/drukarka1.png" alt="Drukarka" title="Drukarka"></a> '+ 
+    '<a href="dysk.html"><img src="pliki_menu/dysk.png" alt="Dysk" title="Dysk"></a> '+ 
+    '<a href="klawiatura.html"><img src="pliki_menu/klawiatura.png" alt="Klawiatura" title="klawaitura"></a> '+
+    '</header>'+
+    '<section style="background-color: rgb(255, 235, 205); padding: 10px; min-height: 400px;">'
+    );
+    
