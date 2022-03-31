adde747ce0d7cf689a7b479aac43bcc079fbcaca
artisticoinformatico <96482901+artisticoinformatico@users.noreply.github.com>
2022-03-31 14:29:01 +0200

    wyprowadzka

--- Mateusz Melon/boomer/footer.js
+++
@@ -1,1 +0,0 @@
-document.write('</section><footer><a href="index.html">index</a></footer>')
--- Mateusz Melon/footer.js
+++
@@ -1,8 +0,0 @@
-document.write(
-    '</section>'+
-    '<footer style="text-align:center; background-color: rgb(218, 144, 34); padding: 25px;">'+
-    '<a href=index.html> Index </a>'+ 
-    '<p id="kom"></p>'+ 
-    '</footer>'+
-    '</div>'
-        );
